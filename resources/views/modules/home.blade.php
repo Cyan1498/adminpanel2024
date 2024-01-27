340baa468dcdd4a53d7bde1b5ec3012c64316c11
Cyan <yancerna1498@gmail.com>
2024-01-27 17:56:41 -0500

    Modificacion vistas

--- resources/views/modules/home.blade.php
+++ resources/views/modules/home.blade.php
@@ -1,14 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
+@extends('layouts.themes.app')
 
+@section('content')
+{{-- <div class="section"> --}}
+<div class="section-header mb-0">
     <h1>BIENVENIDO 2024</h1>
 
-</body>
-</html>
+    <div class="section-body">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body p-2">
+                        {{-- <h1>QUE FUE</h1> --}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    </div>
+</div>
+
+
+@endsection
--- resources/views/modules/user/index.blade.php
+++ resources/views/modules/user/index.blade.php
@@ -2,30 +2,30 @@
 
 @section('content')
 {{-- <div class="section"> --}}
-    <div class="section-header mb-0">
-        <h1>Lista de Usuarios</h1>
+<div class="section-header mb-0">
+    <h1>Lista de Usuarios</h1>
 
-        {{-- <div class="section-header-breadcrumb">
+    {{-- <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
                 <div class="breadcrumb-item"><a href="#">Components</a></div>
                 <div class="breadcrumb-item">Table</div>
             </div> --}}
-        <div class="section-header-breadcrumb">
-            <a class="btn btn-sm btn-primary mr-5" href="{{ route('users.create') }}"><i class="fa fa-add"></i>add New</a>
-            
-        </div>
+    <div class="section-header-breadcrumb">
+        <a class="btn btn-sm btn-primary mr-5" href="{{ route('users.create') }}"><i class="fa fa-add"></i>Añadir usuario</a>
+
     </div>
-    <div class="section-body">
-        {{-- <h2 class="section-title">Table</h2>
+</div>
+<div class="section-body">
+    {{-- <h2 class="section-title">Table</h2>
             <p class="section-lead">Example of some Bootstrap table components.</p> --}}
-    
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="card">
-    
-                    {{-- <div class="card-header"> --}}
-                    {{-- <h4>Advanced Table</h4> --}}
-                    {{-- <div class="card-header-form">
+
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+
+                {{-- <div class="card-header"> --}}
+                {{-- <h4>Advanced Table</h4> --}}
+                {{-- <div class="card-header-form">
                                 <form>
                                     <div class="input-group">
                                         <input type="text" class="form-control" placeholder="Search">
@@ -35,51 +35,53 @@
                                     </div>
                                 </form>
                             </div> --}}
-                    {{-- </div> --}}
-                    <div class="card-body p-2">
-                        <div class="table-responsive">
-                            <table class="table table-hover" style="background-color: hsl(227, 34%, 15%);" id="tblExports">
-                                <thead style="background-color: hsl(227, 34%, 15%);">
-                                    <tr>
-                                        <th>Nombre</th>
-                                        <th>Apellido</th>
-                                        <th>Edad</th>
-                                        <th>Email</th>
-                                        <th>Phone</th>
-                                        <th>Country</th>
-                                        <th></th>
-                                        {{-- <th></th> --}}
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($users as $id => $user)
-                                    <tr>
-                                        <td style="color: #fff;">{{ $user['name'] }}</td>
-                                        <td style="color: #fff;">{{ $user['lastname'] }}</td>
-                                        <td style="color: #fff;">{{ $user['age'] }}</td>
-                                        <td style="color: #fff;">{{ $user['email'] }}</td>
-                                        <td style="color: #fff;">{{ $user['phone'] }}</td>
-                                        <td style="color: #fff;">{{ $user['country'] }}</td>
-                                        <td style="color: #fff;"><a href="{{ route('users.edit', $user['id']) }}" class="btn btn-success btn-sm btn-rounded">Edit</a></td>
-                                        {{-- <h2>{{ $user['id']}}</h2> --}}
-    
-                                        {{-- <form action="{{ route('users.destroy', ['user' => $id]) }}" method="POST">
-                                        @method('DELETE')
-                                        @csrf
-                                        <td><button type="submit" class="btn btn-danger btn-sm btn-rounded">Delete</button></td>
-                                        </form> --}}
-                                    </tr>
-                                    @endforeach
-                                </tbody>
-    
-                            </table>
-                        </div>
+                {{-- </div> --}}
+                <div class="card-body p-2">
+                    <div class="table-responsive">
+                        <table class="table table-hover" style="background-color: hsl(227, 34%, 15%);" id="tblExports">
+                            <thead style="background-color: hsl(227, 34%, 15%);">
+                                <tr>
+                                    <th style="color: #fff;">NOMBRE</th>
+                                    <th style="color: #fff;">APELLIDO</th>
+                                    <th style="color: #fff;">EDAD</th>
+                                    <th style="color: #fff;">EMAIL</th>
+                                    <th style="color: #fff;">TELEFONO</th>
+                                    <th style="color: #fff;">PAIS</th>
+                                    <th style="color: #fff;">ACCIONES</th>
+                                    {{-- <th></th> --}}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($users as $id => $user)
+                                <tr>
+                                    <td style="color: #fff;">{{ $user['name'] }}</td>
+                                    <td style="color: #fff;">{{ $user['lastname'] }}</td>
+                                    <td style="color: #fff;">{{ $user['age'] }}</td>
+                                    <td style="color: #fff;">{{ $user['email'] }}</td>
+                                    <td style="color: #fff;">{{ $user['phone'] }}</td>
+                                    <td style="color: #fff;">{{ $user['country'] }}</td>
+                                    {{-- <h2>{{ $user['id']}}</h2> --}}
+
+                                    <td>
+                                        <form action="{{ route('users.destroy', $user['id']) }}" method="POST">
+                                            <a href="{{ route('users.edit', $user['id']) }}" class="btn btn-success btn-sm btn-rounded"><i class="fa fa-pen"></i></a>
+                                            @csrf
+                                            @method('DELETE')
+                                            <button type="submit" class="btn btn-sm btn-danger"><i class="fa fa-trash"></i></button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
-    
     </div>
+
+</div>
 {{-- </div> --}}
 {{-- @section('js')
     <script>
@@ -97,7 +99,7 @@
 
     @endsection --}}
 
-    {{-- <script>
+{{-- <script>
         $(document).ready(function () {
             $('#tblExports').DataTable({
                 responsive: true,
@@ -110,4 +112,3 @@
         });
     </script> --}}
 @endsection
-
